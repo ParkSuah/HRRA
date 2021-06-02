28b88d9038d4ef4ee38e72a792d34c2c50608c5a
ParkSuah <48156241+ParkSuah@users.noreply.github.com>
2021-06-02 23:57:23 +0900

    card

--- lib/posting.dart
+++ lib/posting.dart
@@ -14,19 +14,48 @@ class PostingPage extends StatefulWidget {
 }
 
 class _PostingPageState extends State<PostingPage> {
-
   Widget _buildBody(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance.collection('posting').snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
-        return new ListView(
-          children: snapshot.data.docs.map((DocumentSnapshot document) {
-            return new ListTile(
-              title: new Text(document.data()['title']),
-              subtitle: new Text(document.data()['level']),
-            );
-          }).toList(),
+        return new GridView.count(
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 5,
+              crossAxisCount: 5,
+              children: snapshot.data.docs.map((DocumentSnapshot document) {
+                return new Expanded(
+                  child: Card(
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),),
+                      child: Container(
+                        width: 200,
+                        height: 250,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Expanded(child: Text("Title   "+document.data()['Title'])),
+                            Expanded(child: Text("Level   "+document.data()['Level'].toString())),
+                            Expanded(child: Text("Post    "+document.data()['Post'] )),
+                            Expanded(child: Text("Division  "+document.data()['Division'])),
+                            Expanded(child: Text("Branch  "+document.data()['Branch'])),
+                            Expanded(child: Text("Duty station  "+document.data()['Duty'])),
+                            Expanded(child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                TextButton(onPressed: (){
+                                  print("show applicant");
+                                },
+                                    child: Expanded(child: Text("Applicant")),),
+                                    Expanded(flex: 5, child: SizedBox(width: 50,)),
+                              ],
+                            ),)
+                          ],
+                        ),
+                      )
+                  ),
+                );
+              }).toList(),
         );
       },
     );
