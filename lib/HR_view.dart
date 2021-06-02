5825d2dbe8551a984d8bd05aedcd7c81ef0135ee
ParkMinjoo <21800268@handong.edu>
2021-06-02 23:23:30 +0900

    add selected checkbox

--- lib/HR_view.dart
+++ lib/HR_view.dart
@@ -24,43 +24,39 @@ class _HRviewPageState extends State<HRviewPage> {
 }
 
 class MyStatelessWidget extends StatelessWidget {
-  // Offer offer;
-  // MyStatelessWidget({this.offer});
-
   final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('offering').snapshots();
 
   @override
   Widget build(BuildContext context) {
-    // List<DocumentSnapshot> list;
+    // List<DocumentSnapshot> document;
     return StreamBuilder<QuerySnapshot>(
       stream: _usersStream,
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+
         if (snapshot.hasError) {
           return Text('Something went wrong');
         }
         if (snapshot.connectionState == ConnectionState.waiting) {
+          // document = snapshot.data.docs;
           return Text("Loading");
         }
 
-        return new
-        // Expanded(
-        // flex: 4,
-        // child:
-        ListView(
+        return new ListView(
           children: snapshot.data.docs.map((DocumentSnapshot document) {
             // return new ListTile(
             //   title: new Text(document.data()['firstname']),
             //   subtitle: new Text(document.data()['lastname']),
             // );
             return new DataTable(
+
               columns: const <DataColumn>[
-                // DataColumn(
-                //   label: Text(
-                //     'ID#',
-                //     // document.data()['firstname'],
-                //     style: TextStyle(fontStyle: FontStyle.italic),
-                //   ),
-                // ),
+                DataColumn(
+                  label: Text(
+                    'Selected',
+                    // document.data()['firstname'],
+                    style: TextStyle(fontStyle: FontStyle.italic),
+                  ),
+                ),
                 DataColumn(
                   label: Expanded(child: Text(
                     'First name',
@@ -113,10 +109,10 @@ class MyStatelessWidget extends StatelessWidget {
 
               ],
               rows: <DataRow>[
-                // Expanded (
                   DataRow(
                     cells: <DataCell>[
                       // DataCell(new Text(document.data()['ID'])),
+                      DataCell(new Checkbox( value: true, onChanged: (bool value) {}, )),
                       DataCell(Expanded(child: new Text(document.data()['firstname']))),
                       DataCell(Expanded(child:new Text(document.data()['lastname']))),
                       DataCell(Expanded(child:new Text(document.data()['gender']))),
@@ -127,111 +123,11 @@ class MyStatelessWidget extends StatelessWidget {
                       DataCell(Expanded(child:new Text(document.data()['PHP']))),
                     ],
                   ),
-                // ),
               ],
             );
           }).toList(),
-        // )
         );
       },
     );
   }
-
-  /*
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    return new ListView(
-      scrollDirection: Axis.horizontal,
-      children: [
-        DataTable(
-          columns: const <DataColumn>[
-            DataColumn(
-              label: Text(
-                'ID#',
-                // document.data()['firstname'],
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                'First name',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                'Last name',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                'Gender',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                'Nationality',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                'Current Position title',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                'Current position level',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                'Current duty station',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                'PHP',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-            ),
-          ],
-
-          rows: const <DataRow>[
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('12345')),
-                // DataCell(Text(offer.ID)),
-                DataCell(Text('Suah')),
-                DataCell(Text('Park')),
-                DataCell(Text('F')),
-                DataCell(Text('USA')),
-                DataCell(Text('Position1')),
-                DataCell(Text('2')),
-                DataCell(Text('LA')),
-                DataCell(Text('12345.pdf')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('12346')),
-                DataCell(Text('Mark')),
-                DataCell(Text('Lee')),
-                DataCell(Text('M')),
-                DataCell(Text('Canada')),
-                DataCell(Text('Position3')),
-                DataCell(Text('3')),
-                DataCell(Text('Vancouver')),
-                DataCell(Text('12346.pdf')),
-              ],
-            ),
-          ],
-        ),
-      ],
-    );
-  }*/
 }
